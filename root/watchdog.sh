bdf2b4aefbe4ee93565fa500a8eae27813a3018e
Tris <tris.phillips@gmail.com>
2014-10-30 15:44:26 +0000

    tidy up start

--- root/watchdog.sh
+++ root/watchdog.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
 while [ true ]; do
+	date
 	echo Checking . . .
 	res=`ping -c 3 8.8.8.8 | grep -c  64`
 	if [ "$res" != "3" ]; then
-		echo Not OK, restarting interface . . .
+		echo Not OK
+		echo Syslog
+		echo ------
+		tail /var/log/syslog
+		echo
+		echo ------
+		echo
+		echo dmesg
+		echo -----
+		dmesg 
+		echo Restarting interface . . .
 		ifdown usb0
 		ifup usb0
 		sleep 5
 		echo Rechecking . . .
 		res=`ping -c 3 8.8.8.8 | grep -c  64`
         	if [ "$res" != "3" ]; then
-			echo Not OK, rebooting . . .
+			echo Not OK
+			echo Not OK
+                	echo Syslog
+                	echo ------
+                	tail /var/log/syslog
+                	echo
+                	echo ------
+                	echo
+                	echo dmesg
+                	echo -----
+                	dmesg 
+			echo Rebooting . . .
 			reboot
 		fi
 	else
 		echo Internet OK
 	fi
-	sleep 60
+	sleep 90
 done
