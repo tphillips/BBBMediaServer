4ec9073d4d9f13f18ad17da3d1aed797bae94d0e
tphillips <tris.phillips@gmail.com>
2014-10-30 10:28:42 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ su
 echo "optargs=capemgr.disable_partno=BB-BONELT-HDMI,BB-BONELT-HDMIN" >> /boot/uboot/uEnv.txt
 apt-get update
 apt-get install iw hostapd isc-dhcp-server iptables screen mono-runtime libmono2.0-cil wget zip unzip mpd espeak alsa-utils
-cd 
+cp -r etc /
+cp -r root /
 
